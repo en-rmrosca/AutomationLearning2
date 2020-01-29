3e9cbbce10de7b237fc58629e3bb1fd567cbdf45
rmrosca <radumarin.rosca@quoine.com>
2020-01-29 10:16:33 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,30 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int alfa;
+        long nr = 3245234;
+        float decimal = 98.3f;
+        double number = 543.75432;
+        boolean flag = true;
+        char c = '\u00A9';
+
+        alfa = 42;
+        alfa = 54;
+
+        String s = "He said: \"Hello Mark\"";
+        String b = " and goodbye!";
+
+        int i = 5;
+        for(i = 5; i < s.length(); i++) {
+            for (int j = 0; j < b.length(); j++) {
+            }
+            System.out.println(i);
+        }
+        while(true) {
+            System.out.println(i);
+            i++;
+            if (){
+                break;
+            }
+        }
+    }
+}
