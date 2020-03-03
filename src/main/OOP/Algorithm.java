70ecee77951c5481e2e1e3790788b7756b0434d9
rmrosca <radumarin.rosca@quoine.com>
2020-03-03 11:13:12 +0200

    Finish the Algorithm interface

--- src/main/OOP/Algorithm.java
+++ src/main/OOP/Algorithm.java
@@ -1,5 +1,20 @@
 package main.OOP;
 
+import java.util.ArrayList;
+
 public interface Algorithm {
+     boolean isPalindrome(String s);
+
      void stairCase(int n);
+
+     ArrayList<Float> inverseFloatArray(ArrayList<Float> floatArray);
+
+     ArrayList<Integer> bubbleSort(ArrayList<Integer> intArray);
+
+     void getSumOfOddNumbersAndAmountOfEvenNumbers(ArrayList<Integer> array);
+
+     double shortenPiToDigits(int n);
+
+     void voidShortenPiToDigits(int n);
+
 }
